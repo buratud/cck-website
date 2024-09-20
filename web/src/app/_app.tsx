f4447b912458e376b2c71e956a5c6f56508447b2
NickKhunapoj <Nickkipad@gmail.com>
2024-09-20 13:04:17 +0700

    initial navbar implementation
    - initialise main navbar design
    - no font initialisation yet
    - responsive

---
+++ web/src/app/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
+ 
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+ 
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+    </>
+  )
+}
---
+++ web/src/app/testpage/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import Navbar from '../../components/navbar';
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      {/* Other page content */}
+    </>
+  );
+}
