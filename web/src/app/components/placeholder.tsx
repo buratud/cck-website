47cb12d8f8b021b40ce45c2a27f19be18e02b657
NickKhunapoj <Nickkipad@gmail.com>
2024-09-24 17:09:00 +0700

    Remove component hard coding
    - add ability to fill in the components with title and description as wanted.
    - remove hard coding results on this enhancement.

--- web/src/app/components/placeholder.tsx
+++ web/src/app/components/placeholder.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import styles from './placeholder.module.scss';
 
-const Placeholder: React.FC = () => {
+interface PlaceholderProps {
+  title: string;
+  description: string;
+}
+
+const Placeholder: React.FC<PlaceholderProps> = ({ title, description }) => {
   return (
     <div className={styles.container}>
       <img
@@ -10,9 +15,9 @@ const Placeholder: React.FC = () => {
         className={styles.image}
       />
       <div className={styles.textContainer}>
-        <h2 className={styles.title}>Title Placeholder</h2>
+        <h2 className={styles.title}>{title}</h2>
         <p className={styles.description}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et finibus est. Sed consequat euismod tellus at aliquam.
+          {description}
         </p>
         <button className={styles.button}>Button</button>
       </div>
--- web/src/app/testpage/page.tsx
+++ web/src/app/testpage/page.tsx
@@ -12,10 +12,22 @@ export default function Home() {
 
       <div className={styles.pageContainer}>
         <div className={styles.view}>
-          <Placeholder />
-          <Placeholder />
-          <Placeholder />
-          <Placeholder />
+          <Placeholder
+            title="Dynamic Title 1"
+            description="This is the description for placeholder 1."
+          />
+          <Placeholder
+            title="Dynamic Title 2"
+            description="This is the description for placeholder 2."
+          />
+          <Placeholder
+            title="Dynamic Title 3"
+            description="This is the description for placeholder 3."
+          />
+          <Placeholder
+            title="Dynamic Title 4"
+            description="This is the description for placeholder 4."
+          />
         </div>
       </div>
 
