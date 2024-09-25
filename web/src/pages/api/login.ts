5e452f8258de31920e4973a12d6dad11387f9934
I2eNamE <71267514+I2eNamE@users.noreply.github.com>
2024-09-25 21:33:12 +0700

    use bun for hash and match hased password

--- web/src/pages/api/login.ts
+++ web/src/pages/api/login.ts
@@ -1,12 +1,39 @@
+import { secretKey, user_collection } from '@/config/db';
 import type { NextApiRequest, NextApiResponse } from 'next'
- 
-type ResponseData = {
-  message: string
-}
- 
-export default function handler(
+import * as jose from 'jose'
+
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ message: 'Hello from Next.js!' })
-}
+  switch (req.method) {
+    case 'POST':
+      const { username, password } = req.body;
+      const query = {"username" : username};
+      const data = await user_collection.findOne(query);
+      if (data == null){
+        res.status(404).send({"message" : "username or password is incorrect"})
+      }
+      else {
+        const match = await Bun.password.verify(password,data.passowrd)
+        if (match) {
+          const accessJWT = await new jose.SignJWT({"username" : data.username})
+          .setProtectedHeader({ alg: 'HS256' })
+          .setIssuedAt()
+          .setExpirationTime('2h')
+          .sign(secretKey);
+        }
+        else{
+          res.status(404).send({"message" : "username or password is incorrect"})
+        }
+      }
+      // const hash = await Bun.password.hash(password)
+      // res.send(hash)
+      
+      break;
+    default:
+      res.status(404).send({"message" : "path not found"})
+      break;
+  }
+} 
