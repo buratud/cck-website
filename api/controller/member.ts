fb0018a28a7eb045aba108120bd6eb2a67c0342e
I2eNamE <71267514+I2eNamE@users.noreply.github.com>
2024-09-30 07:44:59 +0700

    add member path

--- api/controller/member.ts
+++ api/controller/member.ts
@@ -1,11 +1,23 @@
 import express from "express";
+import database from "../config/db";
+import validaccesstoken from "./middleware";
 
 
 const router = express.Router()
 
-router.get("/",async (req: any, res : any)=> {
-    // data from member all
-   res.send() 
+router.get("/",async (req: express.Request, res : express.Response)=> {
+    const member = database.collection('member');
+    const data = await member.find({}).toArray();
+    res.send(data)
+
+})
+
+router.post("/",validaccesstoken,async (req : express.Request, res :express.Response) => {
+    const {image,name,description} = req.body;
+    const query = {image,name,description}
+    const member = database.collection("member")
+    const data = await member.insertOne(query)
+    res.send(data)
 })
 
 
