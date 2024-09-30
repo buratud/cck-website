c6283b9eb1481f8ce1d25af403796b244073054f
I2eNamE <71267514+I2eNamE@users.noreply.github.com>
2024-09-30 08:01:43 +0700

    add refreshtoken path

--- api/controller/refreshtoken.ts
+++ api/controller/refreshtoken.ts
@@ -5,13 +5,30 @@ const router = express.Router()
 
 router.post("", async (req: any, res: any) => {
     const authHeader = req.headers.authorization
-    if (!authHeader) { res.status(403).json({ msg: 'Unauthorized: Missing access token' }) }
+    if (!authHeader) {
+        res.status(403).json({ msg: 'Unauthorized: Missing access token' })
+        return
+    }
     const token = authHeader.split(' ')[1];
     const secret = new TextEncoder().encode(process.env.refreshkey);
     try {
         const { payload } = await jose.jwtVerify(token, secret);
         console.log('Verified Payload:', payload);
-        res.send(payload)
+
+        const accesskey = new TextEncoder().encode(process.env.accesskey)
+        const refreshkey = new TextEncoder().encode(process.env.refreshkey)
+        const accessjwt = await new jose.SignJWT({
+            "username": payload.username
+        }).setProtectedHeader({ alg: "HS256" }).setIssuedAt().setExpirationTime('2h').sign(accesskey)
+        const refreshjwt = await new jose.SignJWT({
+            "username": payload.username
+        }).setProtectedHeader({ alg: "HS256" }).setIssuedAt().setExpirationTime('1w').sign(refreshkey)
+        res.send({
+            "accesskey": accessjwt,
+            "refreshkey": refreshjwt
+        })
+
+
     } catch (err) {
         console.error('JWT Verification failed:', err);
         res.status(500).send({
