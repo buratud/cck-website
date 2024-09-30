c7df7e691ad6d0c2dd76d0161ce79a9bb6f9dccd
I2eNamE <71267514+I2eNamE@users.noreply.github.com>
2024-09-30 09:18:41 +0700

    add get activity and activity by id

--- api/controller/activity.ts
+++ api/controller/activity.ts
@@ -1,25 +1,37 @@
 import express from "express";
+import database from "../config/db";
+import validaccesstoken from "./middleware";
+import { ObjectId } from "mongodb";
+import { Query } from "mongoose";
 
 const router = express.Router()
 
 
-// router.get("",(req : any , res : any )=> {
-//     // get all data
-//     res.send(data)
-// })
+router.get("",async (req : any , res : any )=> {
+    const activity = database.collection('activity');
+    const data =  await activity.find({}).toArray()
+    res.send(data)
+})
 
 
-// router.get("/:id",(req : any , res : any) => {
-//     const id = req.param
-//     //  get data by id
-//     req.send(data)
-// })
+router.get("/:id",async (req : express.Request , res : express.Response) => {
+    const params = req.params as unknown as string
+    const activity = database.collection('activity')
+    const Objectid = new ObjectId(params)
+    const query = {_id :Objectid }
+    console.log(query);
+    const data = await activity.findOne(query)
+    console.log(data)
+    res.send(data)
+})
 
 
-// router.post("",(req : any ,res : any )=> {
-//     const {} = req.body
-//     // add data into database
-//     res.status(201).send("add data success")
-// })
+router.post("",validaccesstoken,async (req : express.Request ,res : express.Response )=> {
+    const {title, description, image} = req.body
+    const query = {title,description,image}
+    const activity = database.collection('activity');
+    const data =  await activity.insertOne(query)
+    res.status(201).send(data)
+})
 
 export default router
