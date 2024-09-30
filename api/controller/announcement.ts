1583593629295a6fa01ff29844b0f58d566716d6
I2eNamE <71267514+I2eNamE@users.noreply.github.com>
2024-09-30 10:00:46 +0700

    add anouncement path

--- api/controller/announcement.ts
+++ api/controller/announcement.ts
@@ -1,24 +1,35 @@
 import express from "express";
+import database from "../config/db";
+import { ObjectId } from "mongodb";
+import validaccesstoken from "./middleware";
 
 const router = express.Router()
 
-// router.get("",(req : any , res : any )=> {
-//     // get all data
-//     res.send(data)
-// })
+router.get("",async (req : any , res : any )=> {
+    const data = await database.collection('announcement').find({}).toArray()
+    res.send(data)
+})
 
 
-// router.get("/:id",(req : any , res : any) => {
-//     const id = req.param
-//     //  get data by id
-//     req.send(data)
-// })
+router.get("/:id",async (req : any , res : any) => {
+    const params = req.params as unknown as string
+    const activity = database.collection('announcement')
+    const Objectid = new ObjectId(params)
+    const query = {_id :Objectid }
+    console.log(query);
+    const data = await activity.findOne(query)
+    console.log(data)
+    res.send(data)
+})
 
 
-// router.post("",(req : any ,res : any )=> {
-//     const {} = req.body
-//     // add data into database
-//     res.status(201).send("add data success")
-// })
+router.post("",validaccesstoken,async (req : any ,res : any )=> {
+    const {title,description,image} = req.body
+    const query = {title,description,image}
+    const announcement = database.collection('announcement')
+    const data = await announcement.insertOne(query)
+    // add data into database
+    res.status(201).send("add data success")
+})
 
 export default router
