d54edf1983403a95dcbd7df6aec1ffa33e7bf5f2
I2eNamE <71267514+I2eNamE@users.noreply.github.com>
2024-10-10 20:35:10 +0700

    add upload picture

--- api/controller/member.ts
+++ api/controller/member.ts
@@ -1,9 +1,20 @@
 import express from "express";
 import database from "../config/db";
 import validaccesstoken from "./middleware";
+import multer from "multer";
 
 
 const router = express.Router()
+
+const upload = multer({storage : multer.diskStorage({
+    destination(req,file,cb){
+        cb(null,'./pictures/member')
+    },
+    filename(req, file, callback) {
+        callback(null,file.originalname)
+    },
+
+})})
 
 router.get("/",async (req: express.Request, res : express.Response)=> {
     const member = database.collection('member');
@@ -12,10 +23,19 @@ router.get("/",async (req: express.Request, res : express.Response)=> {
 
 })
 
-router.post("/",validaccesstoken,async (req : express.Request, res :express.Response) => {
-    const {image,name,description} = req.body;
-    const query = {image,name,description}
-    const member = database.collection("member")
+router.post("/",validaccesstoken,upload.array('file'),async (req : express.Request, res :express.Response) => {
+    const {name,description = null} = req.body;
+    const files = req.files as unknown as File[]
+    let listfile :String[] = [] as unknown as String[]
+    if (files?.length != 0){
+        for(const data of files){
+            const name = data.destination+'/'+data.originalname
+            listfile.push(name.slice(1))
+        }
+    }
+    console.log(listfile)
+    const query = {name,description,images:listfile}
+    const member = database.collection('member')
     const data = await member.insertOne(query)
     res.send(data)
 })
