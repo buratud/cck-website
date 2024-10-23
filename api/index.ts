74f557a237beeb7da7e8b3dbe469cf7a471cee57
I2eNamE <71267514+I2eNamE@users.noreply.github.com>
2024-10-23 12:58:53 +0700

    add auto create folder pictures when not exsist

--- api/index.ts
+++ api/index.ts
@@ -6,7 +6,9 @@ import announcement from "./controller/announcement"
 import member from "./controller/member"
 import activity from "./controller/activity"
 import refresh from "./controller/refreshtoken"
+import * as fs from 'node:fs';
 const app = express();
+const folderpath = ["./pictures/activity","./pictures/announcement","./pictures/member"]
 
 app.use('/pictures',express.static('pictures'))
 app.use(bodyParser.json());
@@ -27,7 +29,15 @@ app.use("/member",member)
 app.use("/activity",activity)
 app.use("/refresh",refresh)
 
-
+for (const i of folderpath){
+  console.log(i);
+  if (!fs.existsSync(i)) {
+    fs.mkdirSync(i, { recursive: true });
+    console.log('Folder created successfully');
+  } else {
+    console.log('Folder already exists');
+  }
+}
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
