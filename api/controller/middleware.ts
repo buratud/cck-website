1db5dd1ecf011d751f6969860c25f6a7a196af91
Buratud Thumboon <buratud@gmail.com>
2024-11-02 09:59:59 +0700

    Use logger for middleware

--- api/controller/middleware.ts
+++ api/controller/middleware.ts
@@ -1,23 +1,28 @@
 import * as jose from 'jose'
 import express from 'express'
 import 'dotenv/config'
+import { logger } from './config';
 
 
 const validaccesstoken = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const authHeader = req.headers.authorization as string;
     if (!authHeader) { 
-        res.status(403).json({ msg: 'Unauthorized: Missing access token' }) 
+        res.status(403).json({ error: 'No token provided' }); 
         return
     }
-    const token = authHeader.split(' ')[1];
+    const token = authHeader.split(' ').findLast(() => true);
+    if (!token) {
+        res.status(403).json({ error: 'Malformed token' });
+        return
+    }
     // check token by access
     const secret = new TextEncoder().encode(process.env.accesskey);
     try {
         const { payload } = await jose.jwtVerify(token, secret);
-        console.log('Verified Payload:', payload);
+        logger.info(`Access token verified for ${payload.username}`);
         next();
     } catch (err) {
-        console.error('JWT Verification failed:', err);
+        logger.error(`JWT Verification failed: ${err}`);
         res.status(401).json({ message: 'Invalid or expired token' });
     }
 }
